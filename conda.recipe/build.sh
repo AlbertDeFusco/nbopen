bcd4d43f39a97d66e577434fb2cb84effdcd3835
Albert DeFusco <adefusco@anaconda.com>
2023-08-29 09:43:32 -0500

    linux xdg

--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,6 +1,11 @@
-cp $SRC_DIR/nbopen.icns $PREFIX/
-
-python -m pip install .
-python py2app_setup.py py2app \
-  --dist-dir $PREFIX \
-  --iconfile $PREFIX/nbopen.icns
+platform=`uname`
+if [[ "$platform" == "Darwin" ]]; then
+	cp $SRC_DIR/nbopen.icns $PREFIX/
+	
+	python -m pip install .
+	python py2app_setup.py py2app \
+	  --dist-dir $PREFIX \
+	  --iconfile $PREFIX/nbopen.icns
+else
+	python -m pip install .
+fi
--- conda.recipe/post-link.sh
+++ conda.recipe/post-link.sh
@@ -1,1 +1,7 @@
-codesign -fs - --preserve-metadata=entitlements $PREFIX/nbopen.app
+platform=`uname`
+
+if [[ "$platform" == "Darwin" ]]; then
+	codesign -fs - --preserve-metadata=entitlements $PREFIX/nbopen.app
+else
+	$PREFIX/bin/python -m nbopen.install_xdg
+fi
